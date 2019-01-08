5acaff37b782d103f85d8495e3ebe395076f2ec1
Ryan J. Kinnear <Ryan@Kinnear.ca>
2019-01-07 20:02:41 -0500

    Simple, slow, sodoku solver.

---
+++ sodoku.py
@@ -0,0 +1,208 @@
+from copy import deepcopy
+
+N = 9
+M = 3
+
+
+class FeasibilityError(Exception):
+    pass
+
+# Board will store either None if it is empty,
+# an int if the value has been fixed, or a set
+# or other iterable indicating feasible values
+
+
+class Sodoku:
+    def __init__(self, board):
+        self.board = deepcopy(board)
+        assert(len(board) == N)
+        for row in board:
+            assert(len(row) == N)
+
+        self.feasible = None
+        self.reset_feasible()
+        return
+
+    def get_blank_feasible(self):
+        return [[None] * N for _ in range(N)]
+
+    def reset_feasible(self, i_j=None):
+        if i_j is None:
+            self.feasible = self.get_blank_feasible()
+        else:
+            i, j = i_j
+            for jx in range(N):
+                self.feasible[i][jx] = None
+            for ix in range(N):
+                self.feasible[ix][j] = None
+            for ix, jx in self.iter_box_indices(i, j):
+                self.feasible[ix][jx] = None
+        return
+
+    def __str__(self):
+        thick_row = "||=|=|=||=|=|=||=|=|=||\n"
+        s = ""
+        for i in range(N):
+            if i % M == 0:
+                s += thick_row
+
+            for j in range(N):
+                if j % M == 0:
+                    sep = "||"
+                else:
+                    sep = "|"
+
+                if self.board[i][j] is None:
+                    s += sep + " "
+                else:
+                    s += sep + str(self.board[i][j])
+            s += "||\n"
+        s += thick_row
+        return s
+
+    def iter_row(self, i):
+        for j in range(N):
+            yield self.board[i][j]
+        return
+
+    def iter_col(self, j):
+        for i in range(N):
+            yield self.board[i][j]
+        return
+
+    def iter_box(self, i, j):
+        for (ix, jx) in self.iter_box_indices(i, j):
+            yield self.board[ix][jx]
+        return
+
+    def iter_box_indices(self, i, j):
+        box_row = i // M
+        box_col = j // M
+        for i in range(M):
+            for j in range(M):
+                yield (M * box_row + i, M * box_col + j)
+        return
+
+    def find_feasible(self, i, j):
+        if self.feasible[i][j] is None:
+            feasible = set(list(range(1, N + 1)))
+
+            def _filter_feasible(iterable):
+                nonlocal feasible
+                for v in iterable:
+                    if v in feasible and type(v) is int:
+                        feasible -= {v}
+                return
+
+            _filter_feasible(self.iter_row(i))
+            _filter_feasible(self.iter_col(j))
+            _filter_feasible(self.iter_box(i, j))
+            self.feasible[i][j] = feasible
+        else:
+            feasible = self.feasible[i][j]
+        return feasible
+
+    def is_valid(self):
+        return (all([self.is_valid_row(i) for i in range(N)]) and
+                all([self.is_valid_col(j) for j in range(N)]) and
+                all([self.is_valid_box(M * i, M * j) for i in range(M)
+                     for j in range(M)]))
+
+    def check_feasible(self):
+        return not any(self.board[i][j] is None and
+                       len(self.find_feasible(i, j)) == 0
+                       for i in range(N) for j in range(N))
+
+    def is_valid_row(self, i):
+        row = list(self.iter_row(i))
+        row = self.get_fixed(row)
+        return len(row) == len(set(row))
+
+    def is_valid_col(self, j):
+        col = list(self.iter_col(j))
+        col = self.get_fixed(col)
+        return len(col) == len(set(col))
+
+    def is_valid_box(self, i, j):
+        box = list(self.iter_box(i, j))
+        box = self.get_fixed(box)
+        return len(box) == len(set(box))
+
+    def get_fixed(self, iterable):
+        return [it for it in iterable
+                if type(it) is int]
+
+    def is_feasible(self, i, j, v):
+        feasible = self.find_feasible(i, j)
+        return v in feasible
+
+    def update(self, i, j, v):
+        if not self.is_feasible(i, j, v):
+            raise ValueError("Value {} is not feasible for ({}, {})"
+                             "".format(v, i, j))
+        self.board[i][j] = v
+        self.reset_feasible((i, j))
+        self.fill_determined()
+        return
+
+    def get_feasible_ordered(self):
+        all_feasible = []
+        for i in range(N):
+            for j in range(N):
+                if self.board[i][j] is None:
+                    f = self.find_feasible(i, j)
+                    all_feasible.append((len(f), (i, j)))
+        return [f[1] for f in sorted(all_feasible)]
+
+    def iter_unfilled(self):
+        for i in range(N):
+            for j in range(N):
+                if self.board[i][j] is None:
+                    yield (i, j)
+
+    def fill_determined(self):
+        recall = False
+        for i in range(N):
+            for j in range(N):
+                if self.board[i][j] is None:
+                    f = self.find_feasible(i, j)
+                    if len(f) == 1:
+                        self.board[i][j] = next(iter(f))
+                        self.reset_feasible((i, j))
+                        recall = True
+                        break
+            if recall:
+                break
+        if recall:
+            self.fill_determined()
+        return
+
+    def copy(self):
+        assert self.is_valid(), "Bug, invalid state!"
+        new = Sodoku(self.board)
+        return new
+
+    def is_solved(self):
+        if len(self.get_feasible_ordered()) == 0:
+            return True
+        else:
+            return False
+
+
+def solve(sodoku):
+    if sodoku.is_solved():
+        return sodoku
+
+    # feasible_moves = sodoku.get_feasible_ordered()
+    # for i, j in feasible_moves:
+    for i, j in sodoku.iter_unfilled():
+        for v in sodoku.find_feasible(i, j):
+            new_board = sodoku.copy()
+            new_board.update(i, j, v)
+            if not new_board.check_feasible():
+                continue
+
+            solved = solve(new_board)
+            if solved is not None:
+                return solved
+    return None
---
+++ test_sodoku.py
@@ -0,0 +1,136 @@
+from sodoku import (N, M, Sodoku, solve, FeasibilityError)
+from unittest import TestCase
+
+
+class TestSodoku(TestCase):
+    easy_board = [[1, None, 7, 8, None, None, 6, None, None],
+                  [None, 5, None, None, 1, 3, None, None, 9],
+                  [None, 8, None, 6, None, None, 7, None, 5],
+                  [3, None, 9, 4, 5, None, None, None, None],
+                  [None, 2, None, None, None, None, None, 5, 4],
+                  [None, None, 4, None, None, 2, 8, None, 6],
+                  [None, 1, 8, None, 9, 7, None, None, None],
+                  [None, None, None, 5, None, None, 9, 6, None],
+                  [9, None, None, None, 6, None, 2, None, 8]]
+
+    medium_board = [[None, None, 4, 8, None, 7, None, None, None],
+                    [None, 3, 2, 5, None, 4, None, 6, 7],
+                    [5, None, None, None, None, 3, None, None, None],
+                    [None, 5, None, None, 4, None, None, None, None],
+                    [4, None, 9, None, None, None, 5, None, 2],
+                    [None, None, None, None, 5, None, None, 4, None],
+                    [None, None, None, 9, None, None, None, None, 5],
+                    [2, 6, None, 4, None, 5, 7, 3, None],
+                    [None, None, None, 6, None, 1, 2, None, None]]
+
+    hard_board = [[2, None, None, None, None, None, None, None, None],
+                  [None, None, None, 7, None, None, 3, None, 4],
+                  [None, 3, None, 1, None, 5, None, 2, None],
+                  [None, None, 8, None, 3, 4, None, None, 9],
+                  [None, 9, None, None, None, None, None, 6, None],
+                  [6, None, None, 8, 9, None, 1, None, None],
+                  [None, 5, None, 4, None, 1, None, 8, None],
+                  [3, None, 6, None, None, 9, None, None, None],
+                  [None, None, None, None, None, None, None, None, 7]]
+
+    extreme_board = [[9, None, 5, None, None, 3, None, 2, None],
+                     [2, None, None, None, 7, None, None, None, None],
+                     [None, None, None, 2, None, None, 5, 7, None],
+                     [None, None, 9, None, None, 5, None, None, None],
+                     [None, 6, 8, None, None, None, 7, 4, None],
+                     [None, None, None, 6, None, None, 3, None, None],
+                     [None, 9, 6, None, None, 4, None, None, None],
+                     [None, None, None, None, 1, None, None, None, 6],
+                     [None, 4, None, 3, None, None, 8, None, 1]]
+
+    invalid_board = [[1, None, 7, 8, None, None, 6, None, None],
+                     [None, 5, None, None, 1, 3, None, None, 9],
+                     [None, 8, 8, 6, None, None, 7, None, 5],
+                     [3, None, 9, 4, 5, None, None, None, None],
+                     [None, 2, None, None, None, None, None, 5, 4],
+                     [None, None, 4, None, None, 2, 8, None, 6],
+                     [None, 1, 8, None, 9, 7, None, None, None],
+                     [None, None, None, 5, None, None, 9, 6, None],
+                     [9, None, None, None, 6, None, 2, None, 8]]
+
+    def test000(self):
+        board = self.easy_board
+        str(Sodoku(board))
+        return
+
+    def test_iter_row(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        self.assertTrue({2, 5, 4}, set(s.get_fixed(s.iter_row(4))))
+        return
+
+    def test_iter_col(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        self.assertTrue({1, 5, 9, 6}, set(s.get_fixed(s.iter_col(4))))
+        return
+
+    def test_iter_box(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        self.assertTrue({4, 5, 2}, set(s.get_fixed(s.iter_box(4, 4))))
+        return
+
+    def test_valid_001(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        self.assertTrue(s.is_valid())
+        return
+
+    def test_valid_002(self):
+        board = self.invalid_board
+        s = Sodoku(board)
+        self.assertFalse(s.is_valid())
+        return
+
+    def test_feasible(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        feasible = set(s.find_feasible(4, 4))
+        self.assertEqual(feasible, {3, 7, 8})
+        return
+
+    def test_copy(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        s.copy()
+        return
+
+    def test_update(self):
+        board = self.easy_board
+        s = Sodoku(board)
+        i, j = s.get_feasible_ordered()[0]
+        f = s.find_feasible(i, j)
+        s.update(i, j, next(iter(f)))
+
+        self.assertTrue(s.is_valid())
+        return
+
+    def test_solve000(self):
+        board = self.easy_board
+        s = solve(Sodoku(board))
+        self.assertTrue(s.is_solved())
+        return
+
+    def test_solve001(self):
+        board = self.medium_board
+        s = solve(Sodoku(board))
+        self.assertTrue(s.is_solved())
+        return
+
+    def test_solve002(self):
+        board = self.hard_board
+        s = solve(Sodoku(board))
+        self.assertTrue(s.is_solved())
+        return
+
+    def test_solve003(self):
+        board = self.extreme_board
+        s = solve(Sodoku(board))
+        self.assertTrue(s.is_solved())
+        return
